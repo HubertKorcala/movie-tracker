0e49d9306141a0d3c9003e71f6a9fede7839cece
HubertKorcala <korcalahubert@gmail.com>
2023-11-29 22:27:00 +0100

    feat: add Register page & form

---
+++ components/forms/Register.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import { RegisterSchema } from '@/lib/validations';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '../ui/input';
+
+const Register = () => {
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: '',
+      name: '',
+      password: '',
+      confirm: '',
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof RegisterSchema>) {
+    const { email, name, password } = values;
+
+    const response = await fetch('/api/auth/register', {
+      method: 'POST',
+      body: JSON.stringify({ email, name, password }),
+    });
+    console.log({ response });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Enter password"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                Password must be at least 8 characters long.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirm"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Enter confirmed password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Register</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default Register;
---
+++ lib/validations.ts
@@ -0,0 +1,25 @@
+import * as z from 'zod';
+
+export const RegisterSchema = z
+  .object({
+    email: z.string().email().max(60, 'Email must be less than 60 characters'),
+    name: z
+      .string()
+      .min(3, 'Name must be at least 3 characters')
+      .max(60, 'Name must be less than 60 characters')
+      .regex(/^[a-zA-Z\s]*$/, 'Name cannot have numbers or special characters'),
+    password: z
+      .string()
+      .trim()
+      .min(8, 'Password must be at least 8 characters')
+      .regex(
+        /^(?=.*[A-Z])(?=.*\d).*$/,
+        'Password must contain at least one uppercase letter and one number'
+      ),
+    confirm: z.string().trim().min(8, 'Password must be at least 8 characters'),
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: "Passwords don't match",
+    path: ['confirm'],
+  });
+
