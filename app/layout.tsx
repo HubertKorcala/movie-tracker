bdaffbba430d26f6b02cd7d96123a6e9f950d042
HubertKorcala <korcalahubert@gmail.com>
2023-11-29 22:29:22 +0100

    feat: add SessionProvider & install bcrypt, vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 // eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
+import { getServerSession } from 'next-auth';
+import SessionProvider from '@/context/SessionProvider';
 
 export const metadata: Metadata = {
   title: 'Movie Tracker',
@@ -19,15 +21,17 @@ const spaceGrotesk = Space_Grotesk({
   variable: '--font-spaceGrotesk',
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
+
   return (
     <html lang="en">
       <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-        {children}
+        <SessionProvider session={session}>{children}</SessionProvider>
       </body>
     </html>
   );
